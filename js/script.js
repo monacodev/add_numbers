2a43f2e20fc5a363e03ee09a0aa2ded0a6663fe7
monacodev <nassim.chabane@gmail.com>
2023-05-21 21:47:11 +0200

    ajout d'un message pour afficher les nombres additionné

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 alert("programme qui additionne deux nombres"); // affiche un message dans un popup
 
-
 var nombre = parseInt(prompt("veuillez saisir un nombre")); // convertit une string en number 
 
 console.log(nombre); // affiche dans la console la valeur de nombre 
@@ -9,8 +8,11 @@ console.log(typeof nombre); // affiche le type de la variable nombre. js donne d
 
 var nombre2 = parseInt(prompt("veuillez saisir un nombre2 "));  // convertit une string en number 
 
-console.log(nombre + nombre2);  // affiche l'addition des deux nombres 
+var resultat = nombre + nombre2;  // stockage du résultat de l'addition des deux nombres dans uen variable
 
 var typeOfNb2 = typeof nombre2; // j'ai stocké la valeur de typeof nombre2 pour un coté plus pratique 
 
-console.log("${typeOfNb2}", typeof nombre); 
+console.log("${typeOfNb2}", typeof nombre);
+
+alert(`Le résultat de l'addition de ${nombre} + ${nombre2} = ` + resultat); // j'affiche dans un message le résultat de l'addition de nombre et nombre2  
+
--- readme.md
+++ readme.md
@@ -1,12 +1,13 @@
-# exemple 4 js  programme qui additionne deux nombres 
+# Exemple additionneur js: programme qui additionne deux nombres 
 
 
 ## installation 
 
 <ul>
-  <li> $ git clone https://github.com/monacodev/add_numbers.git </li>
 
- <li>$ cd add_numbers/ </li>
-  
- </ul> 
+    <li> $ git clone https://github.com/monacodev/add_numbers.git </li>
+
+    <li> $ cd add_numbers/ </li>
+    
+</ul> 
 
